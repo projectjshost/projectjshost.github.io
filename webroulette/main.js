6476424771bcaec40fdbe6d98f0fc69b61267515
TechnoByteJS <felixptak06@gmail.com>
2020-10-30 15:25:15 +0100

    web roulette: added difficulties

--- webroulette/main.js
+++ webroulette/main.js
@@ -1,14 +1,43 @@
+//Set Variables
 var plays = 0;
+var diff = 6;
+
+// Roulette Game
 function roulette() {
   plays = plays + 1;
-  var roullete = Math.floor((Math.random() * 6) + 1);
-if (roullete == 1) {
+  var roulette = Math.floor((Math.random() * diff) + 1);
+  // If you lose
+  if (roulette == 1) {
+    // Attempts to crash your browser when you lose.
         while (true) {
             document.title = Math.floor(Math.random() * (99999999999999999 - 11111111111111111) + 11111111111111111);
         }
 }
+// If you win
 else {
-   document.getElementById('1').innerHTML = 'Status: Je hebt geluk!';
-   document.getElementById('2').innerHTML = 'Keer gespeeld: '+ plays;
+   document.getElementById('1').innerHTML = 'Status: You Won!';
+   document.getElementById('2').innerHTML = 'Times Played: '+ plays;
+ }
 }
+// Set the difficulty
+function setdiff(d) {
+  diff = d;
+
+  switch(diff) {
+    case 12:
+      document.getElementById('3').innerHTML = 'Difficulty: Easy';
+      document.getElementById('4').innerHTML = 'Win chance: 91.7%';
+      break;
+    case 6:
+      document.getElementById('3').innerHTML = 'Difficulty: Normal';
+      document.getElementById('4').innerHTML = 'Win chance: 83.3%';
+      break;
+    case 3:
+      document.getElementById('3').innerHTML = 'Difficulty: Hard';
+      document.getElementById('4').innerHTML = 'Win chance: 66.6%';
+      break;
+    default:
+      // code block
+  }
+
 }
