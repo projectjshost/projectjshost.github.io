b09f6c24e339021ee08e68a03128fac4f8b1e1ad
TechnoByteJS <felixptak06@gmail.com>
2020-11-15 16:41:30 +0100

    fixed stuff + added calculator

---
+++ calculator/script.js
@@ -0,0 +1,30 @@
+var input = document.getElementById("display");
+
+//script to detect if enter button is clicked to evaluate the result
+
+input.addEventListener("keyup", function(event) {
+  
+    // Number 13 is the "Enter" key on the keyboard
+  
+    if (event.keyCode == 13) {
+    
+      // Trigger the button element with a click
+      
+      document.getElementById("equal").click();
+  }
+});
+
+// Function to calculate the result
+function calculate(operation) {
+  let result;
+  try {
+    result = eval(operation);  
+  } catch (error) {}
+
+  if ( result === 0 || isFinite(result) && Boolean(result) ) {
+    document.getElementById('display').value = result;
+  } else {
+    document.getElementById('display').value = 'Invalid operation';
+  }
+
+} 
