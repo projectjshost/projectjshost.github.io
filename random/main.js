7d6b90c3933a8d55d4511d4784cd2b0c2bf12de4
technobytejs <technbyte@protonmail.com>
2022-11-13 09:58:28 +0100

    fixes

--- random/main.js
+++ random/main.js
@@ -8,16 +8,9 @@ function randomPassGen() {
     worker.postMessage(PassLen);
 }
 
-function getRandom(min, max) {
-    let min1 = parseInt(min);
-    let max1 = parseInt(max)+1;
-    let num = Math.random() * (max1 - min1) + min1;
-
-    return Math.floor(num);
-};
 function randomNumGen() {
-    let randomNumMin = document.getElementById("randomNumMin").value;
-    let randomNumMax = document.getElementById("randomNumMax").value;
+    let randomNumMin = parseInt(document.getElementById("randomNumMin").value);
+    let randomNumMax = parseInt(document.getElementById("randomNumMax").value)+1;
     let result = getRandom(randomNumMin, randomNumMax);
     document.getElementById("randomNumResult").innerText = result;
 }
