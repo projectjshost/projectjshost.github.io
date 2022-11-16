77a8b4d08496fa117f78e48ba3a298522c0f0424
technobytejs <technbyte@protonmail.com>
2022-11-16 21:45:57 +0100

    update calculator

--- calculator/main.js
+++ calculator/main.js
@@ -1,46 +1,42 @@
 // Project JS Calculator App
-var display
-$(document).ready(function(){ 
-    display = document.getElementById('display');
-});
-
+const display = document.getElementById('display');
 const π = 3.14592;
-var lastresult = 0;
-var dispInit = true;
+let lastresult = 0;
+let dispInit = true;
 
-function calculate(operation) {
-    let result;
-    try {
-      result = eval(operation);  
-    } catch (error) {}
-  
-    if ( result === 0 || isFinite(result) && Boolean(result) ) {
-      document.getElementById('display').innerText = result;
-      lastresult = result;
-    } else {
-      document.getElementById('display').innerText = 'Error';
-    }
-    dispInit = true;
+const calculate = (operation) => {
+	let result;
+	try {
+		result = eval(operation);
+	} catch (error) {}
+
+	if (result === 0 || isFinite(result) && Boolean(result)) {
+		display.innerText = result;
+		lastresult = result;
+	} else {
+		display.innerText = 'Error';
+	}
+	dispInit = true;
 }
 
-function addChar(character) {
-    if(dispInit==true) {
-        display.innerText=character;
-    } else {
-        display.innerText+=character;
-    }
-    dispInit = false;
+const addChar = (character) => {
+	if (dispInit == true) {
+		display.innerText = character;
+	} else {
+		display.innerText += character;
+	}
+	dispInit = false;
 }
 
-function backspace() {
-  if (display.innerText.length>1&&dispInit==false) {
-    display.innerText=display.innerText.slice(0,-1);
-  } else {
-    AC()
-  }
+const backspace = () => {
+	if (display.innerText.length > 1) {
+		display.innerText = display.innerText.slice(0, -1);
+	} else {
+		AC()
+	}
 }
 
-function AC() {
-  display.innerText='0';
-  dispInit = true;
+const AC = () => {
+	display.innerText = '0';
+	dispInit = true;
 }
