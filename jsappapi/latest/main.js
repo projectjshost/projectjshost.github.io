a34463dfdcab849c055e3645d96447774c799a41
projectjshost <73702066+projectjshost@users.noreply.github.com>
2026-02-04 19:55:54 +0100

    improve Random

--- jsappapi/latest/main.js
+++ jsappapi/latest/main.js
@@ -351,3 +351,33 @@ const runInSandboxString = async (code) => {
 
 	return String(output);
 };
+
+
+const sha256 = async (message) => {
+	// encode as UTF-8
+	const msgBuffer = new TextEncoder().encode(message);
+
+	// hash the message
+	const hashBuffer = await crypto.subtle.digest('SHA-256', msgBuffer);
+
+	// convert ArrayBuffer to Array
+	const hashArray = Array.from(new Uint8Array(hashBuffer));
+
+	// convert bytes to hex string
+	const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
+	return hashHex;
+}
+
+const cyrb53 = (str, seed = 0) => {
+	let h1 = 0xdeadbeef ^ seed, h2 = 0x41c6ce57 ^ seed;
+	for (let i = 0, ch; i < str.length; i++) {
+		ch = str.charCodeAt(i);
+		h1 = Math.imul(h1 ^ ch, 2654435761);
+		h2 = Math.imul(h2 ^ ch, 1597334677);
+	}
+	h1 = Math.imul(h1 ^ (h1 >>> 16), 2246822507);
+	h1 ^= Math.imul(h2 ^ (h2 >>> 13), 3266489909);
+	h2 = Math.imul(h2 ^ (h2 >>> 16), 2246822507);
+	h2 ^= Math.imul(h1 ^ (h1 >>> 13), 3266489909);
+	return 4294967296 * (2097151 & h2) + (h1 >>> 0);
+};
--- random/main.js
+++ random/main.js
@@ -1,16 +1,31 @@
-var worker = new Worker('worker.js');
-worker.addEventListener('message', function(e) {
-    document.getElementById("randomPassResult").innerText = e.data;
-});
+function getRandomStr(length, includeUpperCase, includeLowerCase, includeNumbers, includeSymbols) {
+	// Declare all characters
+	let chars = "";
+	if(includeUpperCase) chars += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	if(includeLowerCase) chars += "abcdefghijklmnopqrstuvwxyz";
+	if(includeNumbers) chars += "0123456789";
+	if(includeSymbols) chars += "!@#$%&";
+
+	// Pick characers randomly
+	let str = "";
+	while (length--) {
+		str += chars.charAt(Math.floor(Math.random() * chars.length));
+	}
+	return str;
+};
 
 function randomPassGen() {
-    let PassLen = document.getElementById("randomPassLen").value;
-    worker.postMessage(PassLen);
+	const passwordLength = document.getElementById("randomPassLen").value;
+	const includeUpperCase = document.getElementById("includeUpperCase").checked;
+	const includeLowerCase = document.getElementById("includeLowerCase").checked;
+	const includeNumbers = document.getElementById("includeNumbers").checked;
+	const includeSymbols = document.getElementById("includeSymbols").checked;
+	document.getElementById("randomPassResult").innerText = getRandomStr(passwordLength, includeUpperCase, includeLowerCase, includeNumbers, includeSymbols);
 }
 
 function randomNumGen() {
-    let randomNumMin = parseInt(document.getElementById("randomNumMin").value);
-    let randomNumMax = parseInt(document.getElementById("randomNumMax").value)+1;
-    let result = getRandom(randomNumMin, randomNumMax);
-    document.getElementById("randomNumResult").innerText = result;
+	const randomNumMin = parseInt(document.getElementById("randomNumMin").value);
+	const randomNumMax = parseInt(document.getElementById("randomNumMax").value) + 1;
+	const result = getRandom(randomNumMin, randomNumMax);
+	document.getElementById("randomNumResult").innerText = result;
 }
